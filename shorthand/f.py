b133c73ba730903705abe191e230b44b621fda99
SSripilaipong <SHSnail@mail.com>
2022-05-25 17:23:44 +0700

    feat(filter): test_is_not_x

--- shorthand/f.py
+++ shorthand/f.py
@@ -3,3 +3,7 @@ from typing import Any, Callable
 
 def is_(t: Any) -> Callable[[Any], bool]:
     return lambda x: x is t
+
+
+def is_not(t: Any) -> Callable[[Any], bool]:
+    return lambda x: x is not t
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -3,3 +3,7 @@ from shorthand import F
 
 def test_is_x():
     assert list(filter(F.is_(123), [0, 123, 4, 567, 123])) == [123, 123]
+
+
+def test_is_not_x():
+    assert list(filter(F.is_not(123), [0, 123, 4, 567, 123])) == [0, 4, 567]
