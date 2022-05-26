a9bb40962cdb542e5fde4aa0e0adabd755d5001c
SSripilaipong <SHSnail@mail.com>
2022-05-26 18:12:15 +0700

    docs: make triangle example oneliner

--- examples/triangle.py
+++ examples/triangle.py
@@ -2,10 +2,7 @@ from shorthand.T import *
 
 
 def main(n: int):
-    list(map(
-        do(sub_from(n) >> repeat(" ") >> do(print, end="")) >>
-        do(repeat("*") >> do(print)),
-        range(1, n+1)))
+    list(map(do(sub_from(n) >> repeat(" ") >> do(print, end="")) >> do(repeat("*") >> do(print)), range(1, n+1)))
 
 
 if __name__ == "__main__":
