1796b72e51e08d5efc38765c70750aeef5343650
SSripilaipong <SHSnail@mail.com>
2022-05-26 18:33:49 +0700

    refactor(transformation): rename getitem() -> get()

--- shorthand/T/builder.py
+++ shorthand/T/builder.py
@@ -60,7 +60,7 @@ def repeat(s: str) -> Transformation:
     return Transformation(lambda n: s * n)
 
 
-def getitem(key: Any, default: Any = _EMPTY) -> Transformation:
+def get(key: Any, default: Any = _EMPTY) -> Transformation:
     if default is _EMPTY:
         return Transformation(lambda x: x[key])
     return Transformation(lambda x: x.get(key, default))
--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -82,9 +82,9 @@ def test_repeat_str():
     assert list(map(T.repeat("*"), range(3))) == ["", "*", "**"]
 
 
-def test_getitem():
-    assert list(map(T.getitem("A"), [{"A": 1, "B": 2}, {"C": 3, "A": 4}])) == [1, 4]
+def test_get():
+    assert list(map(T.get("A"), [{"A": 1, "B": 2}, {"C": 3, "A": 4}])) == [1, 4]
 
 
-def test_getitem_with_default_for_dict():
-    assert list(map(T.getitem("A", 999), [{"A": 1, "B": 2}, {}, {"C": 3, "A": 4}])) == [1, 999, 4]
+def test_get_with_default():
+    assert list(map(T.get("A", 999), [{"A": 1, "B": 2}, {}, {"C": 3, "A": 4}])) == [1, 999, 4]
