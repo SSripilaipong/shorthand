984a1f3c455c435e6552b37c114f11f948c7202d
SSripilaipong <SHSnail@mail.com>
2022-05-27 13:05:08 +0700

    docs: fix examples

--- examples/triangle.py
+++ examples/triangle.py
@@ -1,9 +1,9 @@
-from shorthand.transformation import *
+from shorthand.transformation import T
 
 
 def main(n: int):
-    print_triangle = do(sub_from(n) >> repeat(" ") >> do(print, end="")) >> \
-                     do(repeat("*") >> do(print))
+    print_triangle = T.do(T.sub_from(n) >> T.repeat(" ") >> T.do(print, end="")) >> \
+                     T.do(T.repeat("*") >> T.do(print))
     list(map(print_triangle, range(1, n+1)))
 
 
