81d022b130baf3ffb8839db181c9b1fe26ea864a
SSripilaipong <SHSnail@mail.com>
2022-05-27 18:30:15 +0700

    docs: fix fibonacci example

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -1,13 +1,14 @@
 from functools import reduce
 
 from shorthand import A, T
-from shorthand.transformation.transformation import Transformation
 
 
 def main(n: int):
-    fibonacci = A.current() >> T.do(Transformation(lambda x: x[-1] + x[-2]) >> ...)
-    print(reduce(fibonacci, range(n), [0, 1]))
+    initial = [0, 1]
+    fibonacci = A.current() >> T.project([T.item(1), sum])
+
+    print(reduce(fibonacci, range(n), initial)[0])
 
 
 if __name__ == "__main__":
-    main(7)
+    main(10)
