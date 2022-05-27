557282995532b75be11c461f5a6403cbffdd7f5b
SSripilaipong <SHSnail@mail.com>
2022-05-27 15:45:41 +0700

    feat(aggregation): test_mul

--- shorthand/aggregation/builder.py
+++ shorthand/aggregation/builder.py
@@ -1,12 +1,17 @@
+from typing import Callable, Any
+
 from shorthand.aggregation.aggregation import Aggregation
 
 
 class AggregationBuilder:
     def add(self) -> Aggregation:
-        return self._build()
+        return self._build(lambda current, new: current + new)
 
-    def _build(self) -> Aggregation:
-        return Aggregation(lambda current, new: current + new)
+    def mul(self) -> Aggregation:
+        return self._build(lambda current, new: current * new)
+
+    def _build(self, f: Callable[[Any, Any], Any]) -> Aggregation:
+        return Aggregation(f)
 
 
 A: AggregationBuilder = AggregationBuilder()
--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -5,3 +5,7 @@ from shorthand import A
 
 def test_add():
     assert reduce(A.add(), range(4)) == 6
+
+
+def test_mul():
+    assert reduce(A.mul(), range(1, 6)) == 120
